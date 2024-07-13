2c65042e96c862a12b8fb83ff56fe2f67d3bc1f7
NhatQuynhGitHub <nhatquynh011190@gmail.com>
2024-07-14 00:18:56 +0700

    Install customize-cra and babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from '~/component/Button';
 import logo from './logo.svg';
-import './App.css';
+import '~/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/component/Button/index.js
@@ -0,0 +1,6 @@
+import myAlert from "~/myAlert"
+
+
+export default function Button(){
+    return <button onClick={myAlert}>Click me!</button>
+}
