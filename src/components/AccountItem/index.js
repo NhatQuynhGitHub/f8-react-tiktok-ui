a303c9424fa4d75453f6d76983f0faa8645e00aa
NhatQuynhGitHub <nhatquynh011190@gmail.com>
2024-07-22 17:55:49 +0700

    building Header UI #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('avatar')}>
+                {/* <img scr="" alt="" /> */}
+            </div>
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,33 +1,61 @@
-import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+// Tippy
+import Tippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css'; // optional
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+
 import images from '~/assets/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles);
 
 console.log(images.logo);
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([]);
+        }, 0);
+    });
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
-                   <img src={images.logo} alt='Tiktok logo'/>
+                    <img src={images.logo} alt="Tiktok logo" />
                 </div>
-                <div className={cx('search')}>
-                    <input placeholder='Search account and videos' spellCheck='false' />
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search account and videos" spellCheck="false" />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
                         <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
-                <div className={cx('actions')}>
-
-                </div>
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
